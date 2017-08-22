1e03270fa44e11d7f184280f36b9ec03f7347a95
David P. Mayer <dpmayer@usgs.gov>
2017-08-22 15:22:59 -0700

    Added MLA and async support to ode_get_laser_alt.py

--- Workflow_Scripts/ode_get_laser_alt.py
+++ Workflow_Scripts/ode_get_laser_alt.py
@@ -16,6 +16,8 @@
 import argparse
 from osgeo import gdal,ogr,osr  # 3rd party library
 import requests                 # 3rd party library
+import json
+import re
 import sys
 
 gdal.UseExceptions()
@@ -23,22 +25,34 @@ gdal.UseExceptions()
 def parse_arguments():
     ## Create argument parser
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                 description = """Download LOLA or MOLA shot data within a geographic region using the PDS Geoscience Node's Orbital Data Explorer REST interface. 
+                                     description = """Download LOLA, MLA, or MOLA shot data within a geographic region using the PDS Geoscience Node's Orbital Data Explorer REST interface. 
 The search region may be defined by explicitly listing bounding latitudes and longitudes or by passing a map-projected raster as input.
 The script will parse the server's response and download the 'pts_csv' flavor of CSV product and accompanying PDS3 label.
+LOLA and MOLA queries may be submitted synchronously or asynchronously but MLA queries *must* be submitted asynchronously.
 
 See the ODE REST Manual for detailed information on which fields are contained in the output files for each product type:
 http://oderest.rsl.wustl.edu/""",
                                  epilog = """EXAMPLES:
   Search for LOLA shots based on bounding coordinates
-      %(prog)s moon --coords 44.0 44.1 340.5 340.6 \n
+      %(prog)s target moon --coords 44.0 44.1 340.5 340.6 \n
 
   Search for MOLA shots by passing a GeoTIFF and let the script calculate the lat/lon bounds of the image
-      %(prog)s mars --raster my_projected_image.tif
+      %(prog)s target mars --raster my_projected_image.tif \n
+
+  Search for MLA shots asynchronously and request an email when the job is finished
+      %(prog)s target mercury --async --email user@example.com --raster mercury_image.tif \n
+
+  An asynchronous query will generate a numeric Job ID. This Job ID can be passed to %(prog)s to determine the status of the job.
+  If the job has finished, the script will automatically download the pts_csv CSV and accompanying PDS3 label.
+   %(prog)s status 55117
 
 KNOWN ISSUES:
 Passing in images with a polar stereographic projection or that straddle one of the poles will yield undesirable results. 
-Users should instead manually specificy bounding coordinates for areas over the poles.\n
+Users should instead manually specify bounding coordinates for areas over the poles.\n
+
+Queries of MLA data with minlat and westernlon in the range 0.0-0.1 degrees take an unreasonably long time to complete (>12 hours), 
+even over very small search regions. This is a bug on the ODE side. 
+Users needing MLA data from this area should buffer their search region by at least 0.2 degrees in order to work around this behavior.
 
 The script has no way of determining if a raster passed as input contains data from the same target body 
 specified on the command line. Thus, one could pass in an image of Mars as input but specify "Moon" as the target, 
@@ -47,29 +61,65 @@ Users must take care to pass input images that correspond to the desired output 
 
 """)
 
-    parser.add_argument('target',
-                    choices = ["mars","moon"],
-                    type = str.lower, # effectively make this case insensitive
-                    help = """Name of target body. "Mars" will return MOLA data and "Moon" will return LOLA data. """)
+    subparsers = parser.add_subparsers(dest='mode')
 
-    ## User must specify exactly one of --coords or --raster
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--coords',
-                   nargs = 4,
-                   type = float,
-                   metavar = ('minlat', 'maxlat', 'westernlon', 'easternlon'),
-                   help = """Manually list the bounding coordinates for the query. 
-                           Latitudes may range from -90 to 90 degrees. 
-                           Longitudes may range from -180 to 360 degrees, positive east.""")
-    group.add_argument('--raster',
-                   nargs = 1,
-                   metavar = "file",
-                   help = """Pass a raster named 'file' that contains GDAL-compatible projection information, 
-                           such as a GeoTIFF or Level 2 ISIS3 cube.""")
+    ## Target Mode subparser
+    targetMode = subparsers.add_parser('target', help = "Request laser altimeter data for a given target")
+    targetMode.add_argument('target',
+                            choices = ["mars","mercury","moon"],
+                            type = str.lower, # effectively make this case insensitive
+                            help = """Name of target body. "Mars" will return MOLA data and "Moon" will return LOLA data. """)
+    targetMode.add_argument('--async', '-async',
+                            action='store_true',
+                            # nargs = 1,
+                            # type = str.lower, # effectively make this case insensitive
+                            # metavar = "name@example.com",
+                            help = """Submit query in asynchronous mode.""")
+
+    targetMode.add_argument('--email', '-email',
+                            nargs=1,
+                            type = str.lower, # effectively make this case insensitive
+                            metavar = "name@example.com",
+                            required=False,
+                            help="Send email to this address when the job finishes. Recommended for asynchronous jobs.")
+
+    ## In Target Mode, there are 2 mutually exclusive options: coords and raster, each with their own required args
+    group = targetMode.add_mutually_exclusive_group(required=True)
+    group.add_argument('--coords', '-coords',
+                       nargs = 4,
+                       type = float,
+                       metavar = ('minlat', 'maxlat', 'westernlon', 'easternlon'),
+                       help = """Manually list the bounding coordinates for the query. 
+                               Latitudes may range from -90 to 90 degrees. 
+                               Longitudes may range from -180 to 360 degrees, positive east.""")
+    group.add_argument('--raster', '-raster',
+                       nargs = 1,
+                       metavar = "file",
+                       help = """Pass a raster named 'file' that contains GDAL-compatible projection information, 
+                               such as a GeoTIFF or Level 2 ISIS3 cube.""")
+    
+    ## Status Mode subparser
+    statusMode = subparsers.add_parser('status', help = "Check the status of a job")
+    statusMode.add_argument('jobid',
+                            nargs = 1,
+                            type = int,
+                            metavar = "JobID",
+                            help = """Check the status of asynchronous query [JobID] and download data if job finished successfully.""")
+
 
     ## Parse the command line arguments
     args = parser.parse_args()
+    if args.mode is None:
+        parser.print_usage()
+        parser.exit()
+
     return args
+
+
+def isValidEmail(email):
+    if len(email) > 4:
+        if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email) != None:
+            return True
 
 def ValidateCoords(coords):
     # Check that coords are within acceptable ranges
@@ -97,12 +147,10 @@ def ValidateCoords(coords):
         print("westernlon must be < easternlon")
         sys.exit(1)
 
-def LonTo360(coords):
+def LonTo360(dlon):
     # Convert longitudes to 0-360 deg
-    coords[2] = ((360 + coords[2]) % 360)
-    coords[3] = ((360 + coords[3]) % 360)
-    # Return updated coords
-    return coords
+    dlon = ((360 + (dlon % 360)) % 360)
+    return dlon
 
 ## Functions GetExtent() and ReprojectCoords() copied from
 ##  https://gis.stackexchange.com/questions/57834/how-to-get-raster-corner-coordinates-using-python-gdal-bindings
@@ -149,85 +197,43 @@ def ReprojectCoords(coords,src_srs,tgt_srs):
         trans_coords.append([x,y])
     return trans_coords
 
+def targetRaster(raster):
+        try:
+            ds = gdal.Open(raster)
+            gt = ds.GetGeoTransform()
+            cols = ds.RasterXSize
+            rows = ds.RasterYSize
+            ext = GetExtent(gt,cols,rows)
 
+            src_srs = osr.SpatialReference()
+            src_srs.ImportFromWkt(ds.GetProjection())
+            if not ds.GetProjection():
+                print("Error: " + raster + " does not appear to have an associated map projection")
+                sys.exit(1)
 
-command_line_args = parse_arguments()
-## Dict to associate target passed on command line with product type the ODE REST interface is expecting
-products = {'mars': 'molapedr',
-            'moon': 'lolardr'}
+            # Define the target SRS as the geographic coord sys of the source SRS
+            # This causes ReprojectCoords() to return lat/lon values
+            tgt_srs = src_srs.CloneGeogCS()
 
-if command_line_args.coords is not None:
-    coords = command_line_args.coords
-    ValidateCoords(coords)
-    LonTo360(coords)
-    print(coords)
-elif command_line_args.raster is not None:
-    try:
-        raster = command_line_args.raster[0]
-        ds = gdal.Open(raster)
-        gt = ds.GetGeoTransform()
-        cols = ds.RasterXSize
-        rows = ds.RasterYSize
-        ext = GetExtent(gt,cols,rows)
+            geo_ext = ReprojectCoords(ext,src_srs,tgt_srs)
+            coords = []
+            coords.append(min(geo_ext[1][1],geo_ext[2][1]))
+            coords.append(max(geo_ext[0][1],geo_ext[3][1]))
+            coords.append(min(geo_ext[0][0],geo_ext[1][0]))
+            coords.append(max(geo_ext[2][0],geo_ext[3][0]))
 
-        src_srs = osr.SpatialReference()
-        src_srs.ImportFromWkt(ds.GetProjection())
-        if not ds.GetProjection():
-            print("Error: " + raster + " does not appear to have an associated map projection")
+        except RuntimeError as e:
+            print(e)
             sys.exit(1)
-        
-        # Define the target SRS as the geographic coord sys of the source SRS
-        # This causes ReprojectCoords() to return lat/lon values
-        tgt_srs = src_srs.CloneGeogCS()
 
-        geo_ext = ReprojectCoords(ext,src_srs,tgt_srs)
-        coords = []
-        coords.append(min(geo_ext[1][1],geo_ext[2][1]))
-        coords.append(max(geo_ext[0][1],geo_ext[3][1]))
-        coords.append(min(geo_ext[0][0],geo_ext[1][0]))
-        coords.append(max(geo_ext[2][0],geo_ext[3][0]))
-        
-        ValidateCoords(coords)
-        LonTo360(coords)
-
-    except RuntimeError as e:
-        print(e)
-        sys.exit(1)
-
-## Careful use of argparse should preclude ever falling through to this point
-##  but an "else" is included here, just in case
-else:
-    print("Must specify either --coords or --raster on commandline")
-    sys.exit(1)
+        return coords
 
 
-payload = {'results': 'v',
-           'output': 'json',
-           'query': products[command_line_args.product] ,
-           'minlat': coords[0] ,
-           'maxlat': coords[1] ,
-           'westernlon': coords[2] ,
-           'easternlon': coords[3] }
 
-print("Submitting query. Please wait...")
-r = requests.get('http://oderest.rsl.wustl.edu/livegds', params=payload)
-print(r.url)
-gdsResults = r.json()
-
-## Report the status returned by ODE and act accordingly
-gdsStatus = gdsResults['GDSResults']['Status']
-print(gdsStatus)
-if gdsStatus == "Success":
-    gdsStatusNote = gdsResults['GDSResults']['StateSummary']['StatusNote']
-    gdsResultFiles = gdsResults['GDSResults']['ResultFiles']['ResultFile']
-
-    print("Results:")
-    print("   Status:  "+ gdsStatus)
-    print("   StatusNote:  "+ gdsStatusNote)
-
+def download_pts(gdsResultFiles):
     for i in gdsResultFiles:
-        # Only download the "pts_csv" flavor of CSV.
-        # See ODE's GDS REST manual for description
+    # Only download the "pts_csv" flavor of CSV.
+    # See ODE's GDS REST manual for description
         if 'pts_csv' in i['URL']:
             fileurl = i['URL']
             filename = fileurl.split('/')[-1]
@@ -237,13 +243,153 @@ if gdsStatus == "Success":
                 for chunk in r.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
+    return
 
-elif gdsStatus == "Error":
-    gdsError = gdsResults['GDSResults']['Error']
-    print("Results:")
-    print("   Status:  "+ gdsStatus)
-    print("   Error:  "+ gdsError)
-    
+
+command_line_args = parse_arguments()
+# print(command_line_args)
+
+
+if command_line_args.mode == 'target':
+    print("Target mode")
+
+    ## Force use of --async when target is Mercury
+    ## (There isn't an easy way to enforce this with argparse because async is otherwise optional)
+    if command_line_args.target == "mercury" and command_line_args.async is False:
+        print("ERROR: Must use --async flag when target is Mercury")
+        sys.exit(2)
+        
+    ## Dict to associate target passed on command line with product type the ODE REST interface is expecting
+    products = {'mars': 'molapedr',
+                'mercury': 'mlardr',
+                'moon': 'lolardr'}
+
+    if command_line_args.raster is not None:
+        coords = targetRaster(command_line_args.raster[0])
+    elif command_line_args.coords is not None:
+        coords = command_line_args.coords
+
+    ValidateCoords(coords)
+    for i in [2,3]:
+        coords[i] = LonTo360(coords[i])
+    print(coords)
+
+    payload = {'results': 'v',
+               'output': 'json',
+               'query': products[command_line_args.target],
+               'minlat': coords[0],
+               'maxlat': coords[1],
+               'westernlon': coords[2],
+               'easternlon': coords[3] }
+        
+    if command_line_args.email is not None:
+        email = command_line_args.email[0]
+        if not isValidEmail(email):
+            print("Error: " + email + "does not appear to be a valid email address")
+            sys.exit(1)
+        else:
+            payload['email'] = email
+
+    if command_line_args.async is True:
+        payload['async'] = "t"
+    else:
+        payload['async'] = "f"
+
+
+elif command_line_args.mode == 'status':
+    print("Attempting to check status of JobId " + command_line_args.jobid[0])
+    payload = {'jobid': command_line_args.jobid[0],
+               'output': 'json'}
 else:
-    print("Unexpected response received from ODE")
-    print(gdsResults)
+    print("PROGRAMMER ERROR: Unable to determine mode")
+    sys.exit(1)
+
+
+## At this point, regardless of which mode was invoked, we should have a payload for the ODE REST query
+print(payload)
+    
+print("Submitting query. Please wait...")
+
+r = requests.get('http://oderest.rsl.wustl.edu/livegds', params=payload)
+print(r.url)
+gdsResults = r.json()
+
+
+### Manage the query response depending on which mode was invoked
+
+## Quick sanity check for status code in response
+try:
+    gdsResults['GDSResults']['Status']
+except KeyError:
+    print("Unable to find Status code in response from server. Exiting...")
+    sys.exit(1)
+else:
+    gdsStatus = (gdsResults['GDSResults']['Status']).upper()
+        
+
+if command_line_args.mode == 'target':
+    print("Results:")
+    if gdsStatus == "SUCCESS":
+        if command_line_args.async is True:
+            jobid = gdsResults['GDSResults']['JobId']
+            print("   Status:  " + gdsStatus)
+            print("   JobID:   " + jobid )
+            if email is not None:
+                print("A message will be sent to " + email + " when the job finishes.")
+            print("Check job status at: ")
+            print("http://oderest.rsl.wustl.edu/livegds/?jobid=" + jobid + "&output=json")
+            print("or by running ")
+            print("        ode_get_laser_alt.py status " +jobid)
+
+            
+        else:
+            gdsStatusNote = gdsResults['GDSResults']['StateSummary']['StatusNote']
+            gdsCount = gdsResults['GDSResults']['Count']
+            gdsResultFiles = gdsResults['GDSResults']['ResultFiles']['ResultFile']
+
+            print("   Status:  " + gdsStatus)
+            print("   Count:   " + gdsCount)
+            print("   StatusNote:  "+ gdsStatusNote)
+
+            download_pts(gdsResultFiles)
+
+    elif gdsStatus == "ERROR":
+        gdsError = gdsResults['GDSResults']['Error']
+        print("   Status:  "+ gdsStatus)
+        print("   Error:  "+ gdsError)
+    else:
+        print("Unexpected response received from ODE REST Service")
+        print(json.dumps(gdsResults, indent=4, sort_keys=True))
+
+elif command_line_args.mode == 'status':
+    print("Results:")
+    if gdsStatus == 'SUCCESS':
+        gdsState = (gdsResults['GDSResults']['StateSummary']['State']).upper()
+        if gdsState == "FINISHED":
+            gdsStatusNote = gdsResults['GDSResults']['StateSummary']['StatusNote']
+            gdsCount = gdsResults['GDSResults']['Count']
+            gdsResultFiles = gdsResults['GDSResults']['ResultFiles']['ResultFile']
+
+            print("   Status:  " + gdsStatus)
+            print("   Count:   " + gdsCount)
+            print("   StatusNote:  "+ gdsStatusNote)
+
+            download_pts(gdsResultFiles)
+            
+        elif gdsState == ( "RUNNING" or "WAITING" or "ERROR" or "UNKNOWN" ):
+            gdsStatusNote = gdsResults['GDSResults']['StateSummary']['StatusNote']
+            print("   Status:  " + gdsState)
+            print("   StatusNote:  "+ gdsStatusNote)
+        else:
+            print("Unexpected State response received from ODE REST Service")
+            print(json.dumps(gdsResults, indent=4, sort_keys=True))
+    elif gdsStatus == "ERROR":
+        gdsError = gdsResults['GDSResults']['Error']
+        print("   Status:  "+ gdsStatus)
+        print("   Error:  "+ gdsError)
+    else:
+        print("Unexpected response received from ODE REST Service")
+        print(json.dumps(gdsResults, indent=4, sort_keys=True))
+else:
+    print("PROGRAMMER ERROR: Unable to determine mode")
+    sys.exit(1)
